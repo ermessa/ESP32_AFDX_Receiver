984823a5cffe4f472a66a8080292e726ae6485b7
Eduardo Messa <87573592+ermessa@users.noreply.github.com>
2025-05-31 23:18:17 -0300

    Added CRC files

---
+++ main/crc32.h
@@ -0,0 +1,23 @@
+#ifndef _CRC32_H_
+#define _CRC32_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// @brief INIT CRC32
+void crc32Init(void);
+
+/// @brief CALCULATE CHECKSUM
+/// @param data INPUT DATA
+/// @param len DATA SIZE
+/// @return CHECKSUM RESULT
+uint32_t crc32(const uint8_t *data, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
